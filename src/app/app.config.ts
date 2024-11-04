de467a24e62e2b3efbf898e7bac219a68f3aab9e
Jean Talar <jeanttalar@gmail.com>
2024-11-03 23:33:53 -0300

    add provider to register default class to material symbols

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { MatIconRegistry } from '@angular/material/icon';
+
+import { routes } from './app.routes';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()],
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    {
+      provide: APP_INITIALIZER,
+      deps: [MatIconRegistry],
+      multi: true,
+      useFactory: (iconRegistry: MatIconRegistry) => {
+        return () =>
+          iconRegistry.setDefaultFontSetClass('material-symbols-outlined');
+      },
+    },
+  ],
 };
